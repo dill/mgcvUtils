5e7aa070687d1c495ae988e71f08f2e228c2e9fd
David Lawrence Miller <github@ninepointeightone.net>
2021-01-06 18:07:13 +0000

    constraint matrix needs to be set!

--- R/spde_smooth.R
+++ R/spde_smooth.R
@@ -46,26 +46,26 @@ smooth.construct.spde.smooth.spec <- function(object, data, knots){
   }
 
   # observation locations
-  dim <- length(object$term) 
+  dim <- length(object$term)
   if (dim > 2 | dim < 1) stop("SPDE Matern can only be fit in 1D or 2D.")
   if (dim == 1) {
     x <- data[[object$term]]
   } else {
-    x <- matrix(0, nrow = length(data[[1]]), ncol = 2) 
+    x <- matrix(0, nrow = length(data[[1]]), ncol = 2)
     x[,1] <- data[[object$term[1]]]
-    x[,2] <- data[[object$term[2]]]  
+    x[,2] <- data[[object$term[2]]]
   }
   # setup mesh or accept user mesh
   if (is.null(object$xt)) {
     if (dim == 1) {
       t <- seq(min(x), max(x), len=object$bs.dim)
-      mesh <- INLA::inla.mesh.1d(loc=t, degree=2, boundary="free") 
+      mesh <- INLA::inla.mesh.1d(loc=t, degree=2, boundary="free")
     } else {
       stop("For 2D, mesh must be supplied as argument xt$mesh in s(...,xt = )")
     }
   } else {
     if (class(object$xt$mesh) != "inla.mesh") stop("xt must be NULL or an inla.mesh object")
-    mesh <- object$xt$mesh 
+    mesh <- object$xt$mesh
   }
   # model matrix: projects parameters to observation locations on mesh 
   object$X <- as.matrix(INLA::inla.spde.make.A(mesh, x))
@@ -75,24 +75,27 @@ smooth.construct.spde.smooth.spec <- function(object, data, knots){
   object$S[[1]] <- as.matrix(inlamats$c1)
   object$S[[2]] <- 2 * as.matrix(inlamats$g1)
   object$S[[3]] <- as.matrix(inlamats$g2)
-  # L is a matrix with a column for each smoothing parameter (tau, kappa) 
-  # and a row for each smoothing matrix (c1, g1, g2). 
-  # The (i,j)^th entry of L contains the power that smoothing parameter i 
-  # is computed to before being multiplied by smoothing matrix j. 
+  # L is a matrix with a column for each smoothing parameter (tau, kappa)
+  # and a row for each smoothing matrix (c1, g1, g2).
+  # The (i,j)^th entry of L contains the power that smoothing parameter i
+  # is computed to before being multiplied by smoothing matrix j.
   # E.g. If (1, 2) has value 4, then smoothing parameter 2 (kappa) is taken
   # to the power 4 before being multiplied by smoothing matrix 1 (c1): i.e. kappa^4*c1
   # All of these computations for each element of L are then summed to create a single
-  # smoothing matrix. 
+  # smoothing matrix.
   object$L <- matrix(c(2,2,2,4,2,0), ncol = 2)
-  # Rank is the basis dimension, it is repeated for each smoothing matrix 
-  object$rank <- rep(object$bs.dim,3)
-  # As kappa > 0, the null space of the Matern SPDE is empty 
-  object$null.space.dim <- 0 
+  # Rank is the basis dimension, it is repeated for each smoothing matrix
+  object$rank <- rep(mesh$n, 3)
+  object$knots <- mesh$n
+  # As kappa > 0, the null space of the Matern SPDE is empty
+  object$null.space.dim <- 0
   # Save the mesh
   object$mesh <- mesh
-  object$df <- ncol(object$X)     # maximum DoF (if unconstrained)
+  object$df <- ncol(object$X)     # maximum DoF
+  # set constraint matrix to have no cols for consistency with INLA
+  object$C <- matrix(0, nrow=0, ncol=object$df)
   # Give object a class
-  class(object) <- "spde.smooth" 
+  class(object) <- "spde.smooth"
   return(object)
 }
 
