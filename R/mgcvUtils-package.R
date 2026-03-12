2765811e9bffd6b6fad78739b39f8ea9dbf61510
Dave Miller <dave.miller@bioss.ac.uk>
2026-03-12 15:40:00 +0000

    fix new package doc rules for roxygen

--- R/mgcvUtils-package.R
+++ R/mgcvUtils-package.R
@@ -1,6 +1,5 @@
 #' Utilities for mgcv
 #'
 #' Here are some utilities to use with `mgcv`. This page will be populated with themes to help users get around.
-#' @name mgcvUtils
-#' @docType package
-NULL
+#' @keywords internal
+"_PACKAGE"
